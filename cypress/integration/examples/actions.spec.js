4aa87fc6cf60f215de022e4b520e8f8ad3ab09f2
yazlai <45852211+yazlai@users.noreply.github.com>
2020-11-09 23:02:56 +0100

    Update actions.spec.js

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -33,7 +33,7 @@ context('Actions', () => {
       // Ignore error checking prior to type
       // like whether the input is visible or disabled
       .type('disabled error checking', { force: true })
-      .should('have.value', 'disabled error checking')
+      .should('have.value', 'disabled error checkingGGGGGGG')
   })
 
   it('.focus() - focus on a DOM element', () => {
